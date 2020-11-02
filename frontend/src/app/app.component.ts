91f784462062097f6280d6238f6735a127397ceb
adarsh-basantwani <adarshbasantwani6@gmail.com>
2020-11-02 18:26:08 +0530

    Add files via upload

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverData: JSON;
+  employeeData: JSON;
+  title = 'frontend';
+  constructor(private httpClient: HttpClient) {
+  }
+
+  sayHi() {
+    this.httpClient.get('http://127.0.0.1:5002/').subscribe(data => {
+      this.serverData = data as JSON;
+      console.log(this.serverData);
+    })
+  }
+
+  getAllEmployees() {
+    this.httpClient.get('http://127.0.0.1:5002/employees').subscribe(data => {
+      this.employeeData = data as JSON;
+      console.log(this.employeeData);
+    })
+  }
+
+  ngOnInit(){
+    this.sayHi()
+    this.getAllEmployees()
+  }
+}
